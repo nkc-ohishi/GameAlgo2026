79f210692012edb4ff6f09350ecec7966d4e93d3
ohishi_MSI2023 <gpu.kenji.ohishi@gmail.com>
2026-05-18 18:12:22 +0900

    確認問題スクリプト製作途中

---
+++ Assets/Lesson/Lesson001/Lesson001.cs
@@ -0,0 +1,19 @@
+// Unity復習ゼミ：Lesson001確認問題
+// 問１．以下の条件でLesson001クラスのメンバ変数を作成せよ
+//       条件：
+//       変数の型：boo
+using UnityEngine;
+
+public class Lesson001 : MonoBehaviour
+{
+
+    void Start()
+    {
+
+	}
+
+    void Update()
+    {
+        
+    }
+}
